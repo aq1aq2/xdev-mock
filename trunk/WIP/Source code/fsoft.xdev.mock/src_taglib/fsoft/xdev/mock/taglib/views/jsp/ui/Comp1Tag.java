cd16a07e702407b590551fa9a5803e21aff229f1
nhanava <nhanava@gmail.com>
2011-12-02 09:13:47 +0000

    nhanld struts taglib

---
+++ trunk/WIP/Source code/fsoft.xdev.mock/src_taglib/fsoft/xdev/mock/taglib/views/jsp/ui/Comp1Tag.java
@@ -0,0 +1,21 @@
+package fsoft.xdev.mock.taglib.views.jsp.ui;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.components.Component;
+import org.apache.struts2.views.jsp.ui.AbstractClosingTag;
+
+import com.opensymphony.xwork2.util.ValueStack;
+
+import fsoft.xdev.mock.taglib.components.Comp1;
+
+
+public class Comp1Tag extends AbstractClosingTag {
+
+	private static final long serialVersionUID = 5577089764622677517L;
+
+	public Component getBean(ValueStack stack, HttpServletRequest req, HttpServletResponse res) {
+        return new Comp1(stack, req, res);
+    }
+}
