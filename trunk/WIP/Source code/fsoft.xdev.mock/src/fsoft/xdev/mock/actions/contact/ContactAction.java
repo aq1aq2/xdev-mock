15e9e68d39a6e078dad5437d604d8d2e2c077356
nhanava@gmail.com <nhanava@gmail.com@695f8acf-07c1-2239-1495-59c176e1b7e1>
2011-12-19 08:25:14 +0000

    nhanld Contact List

--- trunk/WIP/Source code/fsoft.xdev.mock/src/fsoft/xdev/mock/actions/contact/ContactAction.java
+++ trunk/WIP/Source code/fsoft.xdev.mock/src/fsoft/xdev/mock/actions/contact/ContactAction.java
@@ -7,6 +7,8 @@ import com.opensymphony.xwork2.ActionSupport;
 
 import fsoft.xdev.mock.dao.IContactDao;
 import fsoft.xdev.mock.models.Contact;
+import fsoft.xdev.mock.models.ContactList;
+import fsoft.xdev.mock.models.TrustRegion;
 
 public class ContactAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
@@ -14,10 +16,26 @@ public class ContactAction extends ActionSupport {
 	private Contact contact;
 	private IContactDao contactDao;
 	
-	private List<Contact> listModel = new ArrayList<Contact>();
+	private List<ContactList> listModel;
+	
+	private String key;
+	
+	// get how many rows we want to have into the grid - rowNum attribute in the grid
+	private Integer rows = 0;
+	// Get the requested page. By default grid sets this to 1.
+	private Integer page = 0;
+	// sorting order - asc or desc
+	private String sord;
+	// get index row - i.e. user click to sort.
+	private String sidx;
+	// Your Total Pages
+	private Integer total = 0;
+	// All Record
+	private Integer records = 0;
 
+	
 	public String execute() {
-		listModel = contactDao.findAll();
+//		listModel = contactDao.findAll();
 		return SUCCESS;
 	}
 	
@@ -26,8 +44,16 @@ public class ContactAction extends ActionSupport {
 	 * @return action returns listModel
 	 */
 	public String list() {
-		System.out.println("Action: List contact");
-		listModel = contactDao.findAll();
+		int to = (rows * page);
+		int from = to - rows;
+
+		// Count Rows (select count(*) from trust Region)
+		records = contactDao.count();
+		// Your logic to search and select the required data.
+		listModel = contactDao.findRange(from, to);
+		// calculate the total pages for the query
+		total = (int) Math.ceil((double) records / (double) rows);
+		
 		return "list";
 	}
 
@@ -63,12 +89,71 @@ public class ContactAction extends ActionSupport {
 		this.contact = contact;
 	}
 
-	public List<Contact> getListModel() {
+	public List<ContactList> getListModel() {
 		return listModel;
 	}
 
-	public void setListModel(List<Contact> listModel) {
+	public void setListModel(List<ContactList> listModel) {
 		this.listModel = listModel;
+	}
+
+	/* 
+	 * Display grid
+	 */
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public Integer getRows() {
+		return rows;
+	}
+
+	public void setRows(Integer rows) {
+		this.rows = rows;
+	}
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
+	public String getSord() {
+		return sord;
+	}
+
+	public void setSord(String sord) {
+		this.sord = sord;
+	}
+
+	public String getSidx() {
+		return sidx;
+	}
+
+	public void setSidx(String sidx) {
+		this.sidx = sidx;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public Integer getRecords() {
+		return records;
+	}
+
+	public void setRecords(Integer records) {
+		this.records = records;
 	}
 
 }
--- trunk/WIP/Source code/fsoft.xdev.mock/src/fsoft/xdev/mock/dao/imp/ContactDao.java
+++ trunk/WIP/Source code/fsoft.xdev.mock/src/fsoft/xdev/mock/dao/imp/ContactDao.java
@@ -2,10 +2,13 @@ package fsoft.xdev.mock.dao.imp;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import fsoft.xdev.mock.dao.IContactDao;
 import fsoft.xdev.mock.models.Contact;
+import fsoft.xdev.mock.models.ContactList;
 
 public class ContactDao extends HibernateDaoSupport implements IContactDao{
 
@@ -37,17 +40,22 @@ public class ContactDao extends HibernateDaoSupport implements IContactDao{
 	public List<Contact> findAll() {
 		return getHibernateTemplate().find("from Contact");
 	}
-
-//	@Override
-//	public List<Contact> findRange(int from, int to) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+	
+	@Override
+	public List<ContactList> findRange(int from, int to) {
+		Query query = getHibernateTemplate().getSessionFactory()
+				.getCurrentSession().createQuery(
+						"select new fsoft.xdev.mock.models.ContactList(c.contactId , c.surName+ ' '+ c.firstName, c.mobilePhone,c.emailAddress, b.value, c.status) from Contact c left join c.referenceDataByContactType b");
+		query.setFirstResult(from);
+		query.setMaxResults(to - from);
+		
+		return query.list();
+	}
 
 	@Override
 	public int count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return DataAccessUtils.intResult(getHibernateTemplate().find(
+				"select count(*) from Contact"));
 	}
 
 }
