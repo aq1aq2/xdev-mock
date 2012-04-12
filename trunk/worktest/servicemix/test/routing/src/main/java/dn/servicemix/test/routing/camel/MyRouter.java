e3bd0e96ca6a3b8f61f8fbca27d7a2f4ccaaba16
nhanava <nhanava@gmail.com>
2012-04-12 10:01:08 +0000

    Updated, active web service successful

--- trunk/worktest/servicemix/test/routing/src/main/java/dn/servicemix/test/routing/camel/MyRouter.java
+++ trunk/worktest/servicemix/test/routing/src/main/java/dn/servicemix/test/routing/camel/MyRouter.java
@@ -1,5 +1,13 @@
 package dn.servicemix.test.routing.camel;
 
-public class MyRouter {
+import org.apache.camel.builder.RouteBuilder;
+
+public class MyRouter extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("cxf:bean:serviceA_endpoint?dataFormat=PAYLOAD")
+        .to("log:dn.servicemix?showAll=true&multiline=true");
+	}
 
 }
