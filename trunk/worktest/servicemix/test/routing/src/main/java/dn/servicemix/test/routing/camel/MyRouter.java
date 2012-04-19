fefbd53daf33ee62a97aa59ac2c2367f1ddd0805
nhanava@gmail.com <nhanava@gmail.com@695f8acf-07c1-2239-1495-59c176e1b7e1>
2012-04-19 04:33:45 +0000

    Camel routing successful

--- trunk/worktest/servicemix/test/routing/src/main/java/dn/servicemix/test/routing/camel/MyRouter.java
+++ trunk/worktest/servicemix/test/routing/src/main/java/dn/servicemix/test/routing/camel/MyRouter.java
@@ -7,7 +7,26 @@ public class MyRouter extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("cxf:bean:serviceA_endpoint?dataFormat=PAYLOAD")
-        .to("log:dn.servicemix?showAll=true&multiline=true");
+        .to("log:dn.servicemix?showAll=true&multiline=true")
+        .to("bean:jaxbProcessor?method=unmarshall")
+        .choice()
+                .when(header("operationName").isEqualTo("ShowActor"))
+                    .beanRef("serviceA", "showActor")
+        .end()
+        .to("bean:jaxbProcessor?method=marshall")
+	    .to("log:dn.servicemix?showAll=true&multiline=true");
 	}
+	
+	/* 
+	 * Simple log routing for test
+	 * 
+	 * @see org.apache.camel.builder.RouteBuilder#configure()
+	 */
+//	@Override
+//	public void configure() throws Exception {
+//		from("cxf:bean:serviceA_endpoint?dataFormat=PAYLOAD")
+//        .to("log:dn.servicemix?showAll=true&multiline=true")
+//        .end();
+//	}
 
 }
