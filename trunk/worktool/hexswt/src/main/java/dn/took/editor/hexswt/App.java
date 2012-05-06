8a62981e767224cca8fd25c666e3dd02bf6c50a8
nhanava <nhanava@gmail.com>
2012-05-06 03:03:07 +0000

    Create HexSWT tool project

---
+++ trunk/worktool/hexswt/src/main/java/dn/took/editor/hexswt/App.java
@@ -0,0 +1,13 @@
+package dn.took.editor.hexswt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
