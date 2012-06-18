778ebf6a777c15ea2add724509fc416a6b186edb
nhanava@gmail.com <nhanava@gmail.com@695f8acf-07c1-2239-1495-59c176e1b7e1>
2012-06-18 09:37:23 +0000

    Test servicemix with remote service call, xslt functional.

---
+++ trunk/worktest/servicemix/ESBTest/routing/src/test/java/dn/servicemix/test/junit/XmlConverterTest.java
@@ -0,0 +1,35 @@
+package dn.servicemix.test.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import dn.servicemix.test.junit.common.ResourceUtil;
+
+/**
+ * @author NhanLD Created May 28, 2012 $Revision$ $Date$
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContextTest.xml" })
+public class XmlConverterTest {
+	@Autowired
+	ResourceUtil resourceUtil;
+
+	/**
+	 * Test XmlConverter function.
+	 * 
+	 * @author NhanLD
+	 * @throws Exception
+	 *             exception
+	 */
+	@Test
+	public void testXmlConverter() throws Exception {
+//		XmlConverter converter = new XmlConverter();
+//		Object object = converter.unmarshall(resourceUtil.getInputXml()
+//				.getInputStream());
+//		Assert.assertNotNull(TestConstants.FAILED_NULL, object);
+//		Assert.assertTrue(TestConstants.FAILED_NOT_TRUE, object instanceof ShowActorRequest);
+	}
+}
---
+++ trunk/worktest/servicemix/ESBTest/routing/src/test/java/dn/servicemix/test/junit/common/ResourceUtil.java
@@ -0,0 +1,42 @@
+/**
+ * EnterpriseXMLResource.java
+ *
+ * ====================================================================================
+ * $Id$
+ * ====================================================================================
+ */
+package dn.servicemix.test.junit.common;
+
+import org.springframework.core.io.Resource;
+
+/**
+ * @author NhanLD
+ * Created May 28, 2012
+ * $Revision$ $Date$
+ */
+public class ResourceUtil
+{
+    private Resource inputXml;
+
+    /**
+     * Getter for inputXml.
+     *
+     * @author NhanLD
+     * @return the inputXml
+     */
+    public Resource getInputXml()
+    {
+        return inputXml;
+    }
+
+    /**
+     * Setter for inputXml.
+     *
+     * @author NhanLD
+     * @param inputXml the inputXml to set
+     */
+    public void setInputXml(Resource inputXml)
+    {
+        this.inputXml = inputXml;
+    }
+}
